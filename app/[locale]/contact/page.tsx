71d3f42c140c6737c13efdcfec1e9d7dc37a2627
SoroushDev <mr.soroosh.qr@gmail.com>
2024-08-07 11:20:45 +0330

    Fix

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -54,7 +54,7 @@ export default function Page() {
                         </label>
                         <input type="email" id="email"
                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                               placeholder="name@flowbite.com" required
+                               placeholder="name@address.com" required
                                {...register('email', {required: true})}/>
                     </div>
                     <div className="mb-5">
